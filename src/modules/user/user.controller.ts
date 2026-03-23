30d27e6b387cf7e28d761a99a2d01cbf50dc55de
awoladhossain <awoladh04@gmail.com>
2026-03-23 17:49:13 +0600

    user router initial

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,1 +1,19 @@
+import type { Request, Response } from "express";
+import { ApiResponse } from "../../shared/types/response.type";
+import asyncHandler from "../../shared/utils/asyncHandler";
+import { SafeUser } from "./user.interface";
+import { UserService } from "./user.service";
 
+const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
+  const users = await UserService.getAllUsers();
+  const response: ApiResponse<SafeUser[]> = {
+    success: true,
+    message: "Users retrieved successfully",
+    data: users,
+  };
+  res.status(200).json(response);
+});
+
+export const UserController = {
+  getAllUsers,
+};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,13 +1,16 @@
+import type { SafeUser } from "./user.interface";
 import User from "./user.model";
-import type {
-  SafeUser,
-  RegisterInput,
-  UpdateUserInput,
-} from "./user.interface";
-import AppError from "../../shared/utils/AppError";
 
-
-const sanitizeUser =(user:any): SafeUser =>{
+const sanitizeUser = (user: any): SafeUser => {
   const { password, ...safeUser } = user.toObject(); // password ফিল্ড বাদ দিয়ে বাকি সব ফিল্ড safeUser তে রাখবে
   return safeUser as SafeUser; // Type assertion দিয়ে SafeUser টাইপে কাস্ট করা
-}
+};
+
+const getAllUsers = async (): Promise<SafeUser[]> => {
+  const users = await User.find({ status: "active" }); // active ইউজার গুলো খুঁজে বের করবে
+  return users.map(sanitizeUser); // প্রতিটি ইউজারকে sanitizeUser ফাংশনের মাধ্যমে SafeUser তে রূপান্তর করবে
+};
+
+export const UserService = {
+  getAllUsers,
+};
